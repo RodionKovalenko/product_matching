a30a7c93b3ec6231eecb1506c174cf4d46a033a9
Rodion Kovalenko <rodion_kovalenko@yahoo.de>
2021-06-11 07:58:25 +0200

    einrueckung

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import datetime
 from flask import Flask
 from flask import render_template, make_response, Response, request
 from flask_restful import Resource, Api, reqparse
+from sklearn.metrics import calinski_harabasz_score
 from src.webservice import sbert
 
 app = Flask(__name__)
@@ -24,15 +25,16 @@ def start_app():
 def about_action():
     return render_template('about.html')
 
-#when Button "Vergleich" is clicked
+# when Button "Vergleich" is clicked
+
+
 @app.route("/onComparestrings/", methods=['GET', 'POST'])
 def on_compare_strings_action():
     produkt1 = request.args.get('produkt1')[:]
     produkt2 = request.args.get('produkt2')[:]
-
     product_match_result = sbertAPI.calculate_match([produkt1, produkt2])
 
-    return render_template('index.html', product_matching_result=product_match_result, produkt1 = produkt1, produkt2 = produkt2)
+    return render_template('index.html', product_matching_result=product_match_result, produkt1=produkt1, produkt2=produkt2)
 
 
 # Sbert API
