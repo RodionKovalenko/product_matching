f23d82339ff0555e5ec362aeaddf9a1e4706b49c
Rodion Kovalenko <rodion_kovalenko@yahoo.de>
2021-06-24 08:19:02 +0200

    data type for labels

--- data/model.py
+++ data/model.py
@@ -15,8 +15,14 @@ from torch.utils.data import DataLoader
 #Define the model. Either from scratch of by loading a pre-trained model
 model = SentenceTransformer('distilbert-base-nli-mean-tokens')
 
+print(sample_200)
+
+labels = []
+for i in sample_200.match.values:
+    labels.append(float(i))
+
 #Define your train examples. You need more than just two examples...
-train_examples = [InputExample(texts=[sample_200.productname_1.values, sample_200.productname_2.values], label=sample_200.match.values)]
+train_examples = [InputExample(texts=[sample_200.productname_1.values, sample_200.productname_2.values], label=labels)]
 
 #Define your train dataset, the dataloader and the train loss
 train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=16)
