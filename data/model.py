1d5e5e92972c2cc7b3f07d70cdf46cb70e9a03e6
Rodion Kovalenko <rodion_kovalenko@yahoo.de>
2021-06-27 22:30:01 +0200

    training mit dense layer

--- data/model.py
+++ data/model.py
@@ -23,13 +23,14 @@ sample_evaluation = df.sample(n=50, random_state=1)
 # word_embedding_model = SentenceTransformer('data/sbert_trained_model/')
 
 saved_model_path = 'data/sbert_trained_model'
-word_embedding_model = models.Transformer('distilbert-base-uncased')
+word_embedding_model = models.Transformer('sentence-transformers/distilbert-base-nli-mean-tokens')
 
 #pooling model 
 pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension())
 
+print(pooling_model.get_sentence_embedding_dimension())
 #dense model 
-dense_model = models.Dense(pooling_model.get_sentence_embedding_dimension(), out_features=200,  activation_function=nn.Softmax())
+dense_model = models.Dense(pooling_model.get_sentence_embedding_dimension(), out_features=2,  activation_function=nn.Softmax())
 
 model = SentenceTransformer(modules=[word_embedding_model, pooling_model, dense_model])
 train_num_labels = len(sample_200)
@@ -50,39 +51,28 @@ print(sample_200)
 #transfer labels in float, otherwise it will not accepted
 labels = []
 for i in sample_200.match.values:
-    labels.append(str(int(i)))    
-
-# evaluation labels    
-labels_evaluation_str = []
-for i in sample_evaluation.match.values:
-    labels_evaluation_str.append(str(i))
-
-labels_evaluation = []
-for i in labels_evaluation_str:
-    labels_evaluation.append(str(i))
+    labels.append(str(int(i)))   
 
 #Define your train examples. You need more than just two examples...
 train_examples = []
 for sample_index, row in sample_200.iterrows():
     train_examples.append(InputExample(texts=[row.productname_1, row.productname_2], label=int(row.match)))
 
-evaluation_examples = []
-for sample_index, row in sample_evaluation.iterrows():
-    evaluation_examples.append(InputExample(texts=[row.productname_1, row.productname_2], label=int(row.match)))
-
 #Define your train dataset, the dataloader and the train loss
 train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=16)
 train_loss = losses.SoftmaxLoss(model, sentence_embedding_dimension=model.get_sentence_embedding_dimension(), num_labels=train_num_labels)
 
-evaluation_dataloader = DataLoader(evaluation_examples, shuffle=True, batch_size=16)
-
-evaluator = evaluation.LabelAccuracyEvaluator(dataloader = evaluation_dataloader, softmax_model=dense_model)
-                                        
-
-#Tune the modelgit
+#Tune the model
 model.fit(train_objectives=[(train_dataloader, train_loss)],
           epochs=1,
-          warmup_steps=100,    
-          evaluation_steps=1,
+          warmup_steps=100,
+          evaluation_steps=13,
           output_path=saved_model_path)
 
+
+#evaluate model first on trained data set
+for sample_index, row in sample_200[0:10].iterrows():
+    embeddings = model.encode([row.productname_1, row.productname_2])
+    print(embeddings)
+    print(row.match)
+    print()
