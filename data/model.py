839e4d38a351a96f98dbbd498e0577a7dd71e50f
Rodion Kovalenko <rodion_kovalenko@yahoo.de>
2021-06-27 14:18:51 +0200

    sbert training mit dense and pooling layer

--- data/model.py
+++ data/model.py
@@ -1,6 +1,8 @@
-from sentence_transformers import util,SentenceTransformer
+from sentence_transformers import util,SentenceTransformer, models
 from sentence_transformers import evaluation
 from sentence_transformers import SentenceTransformer, InputExample, losses
+import torch
+from torch import nn
 from torch.utils.data import DataLoader
 #model = SentenceTransformer('distiluse-base-multilingual-cased')
 #model = SentenceTransformer('distilbert-base-nli-mean-tokens')
@@ -16,39 +18,71 @@ sample_200=df.sample(n=200, random_state=1)
 #evaluation sample
 sample_evaluation = df.sample(n=50, random_state=1)
 
-print(sample_200)
-
 #Define the model. Either from scratch of by loading a pre-trained model
 
-model = SentenceTransformer('data/sbert_trained_model/')
+# word_embedding_model = SentenceTransformer('data/sbert_trained_model/')
 
+saved_model_path = 'data/sbert_trained_model'
+word_embedding_model = models.Transformer('distilbert-base-uncased')
+
+#pooling model 
+pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension())
+
+#dense model 
+dense_model = models.Dense(pooling_model.get_sentence_embedding_dimension(), out_features=200,  activation_function=nn.Softmax())
+
+model = SentenceTransformer(modules=[word_embedding_model, pooling_model, dense_model])
+train_num_labels = len(sample_200)
+
+import pathlib
+current_abs_path = str(pathlib.Path().resolve())
+saved_model_dir = current_abs_path + '/' + saved_model_path
+
+import os.path
+if os.path.isdir(saved_model_dir):
+    print('model was already saved')
+    model = SentenceTransformer(saved_model_dir)
+else:
+    print('model was not saved')
+
+
+print(sample_200)
 #transfer labels in float, otherwise it will not accepted
 labels = []
 for i in sample_200.match.values:
-    labels.append(float(i))    
-    
+    labels.append(str(int(i)))    
+
 # evaluation labels    
-labels_evaluation = []
+labels_evaluation_str = []
 for i in sample_evaluation.match.values:
-    labels_evaluation.append(float(i))
+    labels_evaluation_str.append(str(i))
+
+labels_evaluation = []
+for i in labels_evaluation_str:
+    labels_evaluation.append(str(i))
 
 #Define your train examples. You need more than just two examples...
 train_examples = []
 for sample_index, row in sample_200.iterrows():
-    train_examples.append(InputExample(texts=[row.productname_1, row.productname_2], label=float(row.match)))
+    train_examples.append(InputExample(texts=[row.productname_1, row.productname_2], label=int(row.match)))
+
+evaluation_examples = []
+for sample_index, row in sample_evaluation.iterrows():
+    evaluation_examples.append(InputExample(texts=[row.productname_1, row.productname_2], label=int(row.match)))
 
 #Define your train dataset, the dataloader and the train loss
 train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=16)
-train_loss = losses.CosineSimilarityLoss(model)
+train_loss = losses.SoftmaxLoss(model, sentence_embedding_dimension=model.get_sentence_embedding_dimension(), num_labels=train_num_labels)
 
-evaluator = evaluation.EmbeddingSimilarityEvaluator(sample_evaluation.productname_1.values, 
-                                                    sample_evaluation.productname_2.values, 
-                                                    labels_evaluation)
+evaluation_dataloader = DataLoader(evaluation_examples, shuffle=True, batch_size=16)
+
+evaluator = evaluation.LabelAccuracyEvaluator(dataloader = evaluation_dataloader, softmax_model=dense_model)
+                                        
 
 #Tune the modelgit
 model.fit(train_objectives=[(train_dataloader, train_loss)],
-          epochs=100,
-          warmup_steps=100,
-          evaluator=evaluator, 
-          evaluation_steps=50,
-          output_path='data/sbert_trained_model')
+          epochs=1,
+          warmup_steps=100,    
+          evaluation_steps=1,
+          output_path=saved_model_path)
+
--- src/webservice/sbert.py
+++ src/webservice/sbert.py
@@ -1,6 +1,12 @@
 from flask import Flask, json, make_response, request
 from flask_restful import Resource, Api
-from sentence_transformers import SentenceTransformer, util
+from torch import nn
+from sentence_transformers import SentenceTransformer, CrossEncoder, util, models
+
+import pathlib
+current_abs_path = str(pathlib.Path().resolve())
+saved_model_path = 'data/sbert_trained_model'
+saved_model_dir = current_abs_path + '/' + saved_model_path
 
 
 class Sbert(Resource):
@@ -15,26 +21,37 @@ class Sbert(Resource):
     def calculate_match(self, sentences):
         # https://www.sbert.net/
         # model = SentenceTransformer('distilbert-base-nli-mean-tokens')
-        model = SentenceTransformer('data/sbert_trained_model')
+        model = SentenceTransformer(saved_model_dir)
 
         # Compute embeddings
+        # embeddings = model.encode(sentences, convert_to_tensor=True)
         embeddings = model.encode(sentences, convert_to_tensor=True)
+        dense_model = models.Dense(model.get_sentence_embedding_dimension(), out_features=200,  activation_function=nn.Softmax())
 
+        print(embeddings)
+        print(dense_model.forward(embeddings))
         # Compute cosine-similarities for each sentence with each other sentence
-        cosine_scores = util.pytorch_cos_sim(embeddings, embeddings)
+        # cosine_scores = util.pytorch_cos_sim(embeddings, embeddings)
 
         # Find the pairs with the highest cosine similarity scores
-        pairs = []
-        for i in range(len(cosine_scores) - 1):
-            for j in range(i+1, len(cosine_scores)):
-                pairs.append({'index': [i, j], 'score': cosine_scores[i][j]})
+        # pairs = []
+        # for i in range(len(cosine_scores) - 1):
+        #     for j in range(i+1, len(cosine_scores)):
+        #         pairs.append({'index': [i, j], 'score': cosine_scores[i][j]})
 
-        # Sort scores in decreasing order
-        pairs = sorted(pairs, key=lambda x: x['score'], reverse=True)
+        # # Sort scores in decreasing order
+        # pairs = sorted(pairs, key=lambda x: x['score'], reverse=True)
         matching_score = {}
 
-        for pair in pairs[0:10]:
-            i, j = pair['index']
-            matching_score = "Matching score: {:.4f}".format(pair['score'])
+        # for pair in pairs[0:10]:
+        #     i, j = pair['index']
+        #     matching_score = "Matching score: {:.4f}".format(pair['score'])
 
         return matching_score
+
+
+sentences = ['hi rodion test', 'hi jonny test2']
+
+bert = Sbert()
+
+bert.calculate_match(sentences)
